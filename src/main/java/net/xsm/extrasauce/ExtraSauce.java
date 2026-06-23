acbe493f120b8b07ef43f9ce4cd5c1c3d99109c5
Prox <sabax412@gmail.com>
2026-06-23 22:51:18 +1000

    Added new "Item" class

--- src/main/java/net/xsm/extrasauce/ExtraSauce.java
+++ src/main/java/net/xsm/extrasauce/ExtraSauce.java
@@ -4,18 +4,24 @@ import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.resources.Identifier;
 
+import net.xsm.extrasauce.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExtraSauce implements ModInitializer {
 	public static final String MOD_ID = "extra-sauce";
-	// random comment
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
+
+
+
+
+
+
 
 	public static Identifier id(String path) {
 		return Identifier.fromNamespaceAndPath(MOD_ID, path);
